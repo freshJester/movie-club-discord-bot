1bacf6b8e3d8fcd759eaf493c798c311c2428c86
Jay <freshjay2000@gmail.com>
2025-12-27 12:34:13 -0800

    Many minor updates

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ import {
 } from 'discord-interactions';
 import { getRandomEmoji, DiscordRequest } from './utils.js';
 import { getShuffledOptions, getResult } from './game.js';
+import { gapiAuth } from './gapi-auth.js'
+
+await gapiAuth();
 
 // Create an express app
 const app = express();
@@ -56,6 +59,40 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
           ]
         },
       });
+    }
+
+    // "test" command
+    if (name === 'test-sheets') {
+      const spreadsheetId = `1iZStqtDMq4S_veeMRIYqrBBIT0l37YHIEaxXLhGMmyc`
+      const range = `A:B`
+      try {
+        gapi.client.sheets.spreadsheets.values.get({
+          spreadsheetId: spreadsheetId,
+          range: range,
+        }).then((response) => {
+          const result = response.result;
+          const numRows = result.values ? result.values.length : 0;
+          console.log(`${numRows} rows retrieved.`);
+          
+          // Send a message into the channel where command was triggered from
+          return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              flags: InteractionResponseFlags.IS_COMPONENTS_V2,
+              components: [
+                {
+                  type: MessageComponentTypes.TEXT_DISPLAY,
+                  // Fetches a random emoji to send from a helper function
+                  content: response
+                }
+              ]
+            },
+          });
+        });
+      } catch (err) {
+        console.log(`error message: ${err.message}`);
+        return;
+      }
     }
 
     console.error(`unknown command: ${name}`);
--- commands.js
+++ commands.js
@@ -26,6 +26,14 @@ const TEST_COMMAND = {
   contexts: [0, 1, 2],
 };
 
+const TEST_SHEETS = {
+  name: 'test-sheets',
+  description: 'Basic command',
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 1, 2],
+}
+
 // Command containing options
 const CHALLENGE_COMMAND = {
   name: 'challenge',
@@ -44,6 +52,6 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const ALL_COMMANDS = [TEST_COMMAND, TEST_SHEETS, CHALLENGE_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
